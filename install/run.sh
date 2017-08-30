9d08347a7991c4f78db3ade58e6c07f9c3238151
terry brady <terrywbrady+C9@gmail.com>
2017-08-30 23:46:04 +0000

    run.sh

--- install/run.sh
+++ install/run.sh
@@ -1,2 +1,2 @@
-java -Dcantaloupe.config=/path/to/cantaloupe.properties -Xmx2g -jar Cantaloupe-3.3.2.war
+java -Dcantaloupe.config=/path/to/cantaloupe.properties -Xmx2g -jar Cantaloupe3.3.2/Cantaloupe-3.3.2.war
 hostname
