6a5a1c8ca3d40b8c53ed2f46a616d897b6e47a6a
Terry Brady <terrywbrady+c9@georgetown.edu>
2017-08-31 00:25:53 +0000

    add basic config

--- install/run.sh
+++ install/run.sh
@@ -1,2 +1,5 @@
-java -Dcantaloupe.config=/path/to/cantaloupe.properties -Xmx2g -jar Cantaloupe3.3.2/Cantaloupe-3.3.2.war
+P=Cantaloupe-3.3.2
+
 hostname
+
+java -Dcantaloupe.config=$P/cantaloupe.properties -Xmx2g -jar $P/Cantaloupe-3.3.2.war
